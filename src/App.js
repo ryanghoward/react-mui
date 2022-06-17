661c71c774926ae25ce4875b0d1f59570ae072b6
Ryan Howard <ryanghoward@protonmail.com>
2022-06-17 12:11:45 -0700

    oops I broke it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import { Add, Settings } from "@mui/icons-material";
+import { Button, Typography, styled } from "@mui/material";
+
 function App() {
+  const BlueButton = styled(Button)(({ theme }) => ({
+    backgroundColor: theme.palette.otherColor.main,
+    color: "#fff",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue",
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white",
+    },
+  }));
+
   return (
     <div>
-      <h1>Hello</h1>
+      <Button variant='text'>Text</Button>
+      <Button
+        startIcon={<Settings />}
+        variant='contained'
+        color='otherColor'
+        size='small'
+      >
+        Settings
+      </Button>
+      <Button
+        startIcon={<Add />}
+        variant='contained'
+        color='success'
+        size='small'
+      >
+        Add New Post
+      </Button>
+      <Button variant='outlined'>Outlined</Button>
+      <Typography variant='h1' component='p'>
+        This uses h1 style, but it is a P tag
+      </Typography>
+      <BlueButton>My Button</BlueButton>
+      <BlueButton>Another Button</BlueButton>
     </div>
   );
 }
